904f3878117779133bf098a64236e22a210a675b
Miggy Pinaroc <migzpinaroc@gmail.com>
2018-09-05 17:35:41 +0800

    Describe the posts style in branch pages - Create a branch_page.scss file and add CSS - Add CSS to the default.scss file - Add CSS to the mobile.scss file

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -21,21 +21,5 @@ module PostsHelper
 
     def post_format_partial_path
         current_page?(root_path) ? 'posts/post/home_page' : 'posts/post/branch_page'
-
-        context '#post_format_partial_path' do
-            it "returns a home_page partial's path" do
-              helper.stub(:current_page?).and_return(true)
-              expect(helper.post_format_partial_path).to (
-                eq 'posts/post/home_page'
-              )
-            end
-          
-            it "returns a branch_page partial's path" do
-              helper.stub(:current_page?).and_return(false)
-              expect(helper.post_format_partial_path).to (
-                eq 'posts/post/branch_page'
-              )
-            end
-          end
     end
 end
